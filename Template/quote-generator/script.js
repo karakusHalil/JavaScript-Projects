7ab721c3d8e3355fb4070c6d4554951f21c5c981
karakusHalil <karakus_halil@outlook.com>
2024-12-31 23:31:00 +0300

    Created random Quote and add css, fetch and display random quote on load

---
+++ Template/quote-generator/script.js
@@ -0,0 +1,24 @@
+let apiQuotes = [];
+
+//Show New Quote
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+         newQuote();
+    }catch(error) {
+        
+    }
+}
+
+// On Load
+
+getQuotes();
