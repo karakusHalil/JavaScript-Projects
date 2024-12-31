bec349be9631011fd584da3f0f24489b5002ec43
karakusHalil <karakus_halil@outlook.com>
2025-01-01 01:21:58 +0300

    Completed random Quote

--- Template/quote-generator/script.js
+++ Template/quote-generator/script.js
@@ -1,14 +1,57 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter-button');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+console.log(loader);
+
 let apiQuotes = [];
+
+//Show Loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 //Show New Quote
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    if(!quote.author){
+        authorText.textContent = 'Unknow';
+    }else{
+        authorText.textContent = quote.author;
+    }
+
+    if(quote.text.length > 100){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote')
+    }
+    //Set Quote, hide loader
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+    complete();
 }
+
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
 
 // Get Quotes From API
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try{
         const response = await fetch(apiUrl);
@@ -18,6 +61,11 @@ async function getQuotes() {
         
     }
 }
+
+//Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 
