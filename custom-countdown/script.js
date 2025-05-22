5e1c23620f5dc13154a7c686881ddf6588a948e0
karakusHalil <karakus_halil@outlook.com>
2025-05-22 18:47:11 +0300

    Add updateDOM which replaces the countdown with script

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,14 +2,50 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countdownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll("span");
+
 let countdownTitle = "";
 let countdownDate = "";
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split("T")[0];
 //if you want to set local time use toLocaleString()
 
 dateEl.setAttribute("min", today);
+
+//Populate Countdown / Complete UI
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+  console.log(distance);
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor(distance / minute / second);
+  console.log(days, hours, minutes, seconds);
+
+  ///Populate Countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+
+  //Hide Input
+  inputContainer.hidden = true;
+  //Show Countdown
+  countdownEl.hidden = false;
+}
 
 //Take Values from Form Input
 function updateCountdown(e) {
@@ -18,6 +54,10 @@ function updateCountdown(e) {
   countdownDate = e.srcElement[1].value;
 
   console.log(countdownTitle, countdownDate);
+  //Get number version of current Date, updateDOM
+  countdownValue = new Date(countdownDate).getTime();
+  console.log("countdown value:", countdownValue);
+  updateDOM();
 }
 
 //Event Listeners
