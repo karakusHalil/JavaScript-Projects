b4d8e9838389697cf9687f29d0699411ea34d90e
karakusHalil <karakus_halil@outlook.com>
2025-05-26 01:10:59 +0300

    Add caching with localStorage

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -15,6 +15,7 @@ let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -32,13 +33,12 @@ function updateDOM() {
   countdownActive = setInterval(() => {
     const now = new Date().getTime();
     const distance = countdownValue - now;
-    console.log(distance);
 
     const days = Math.floor(distance / day);
     const hours = Math.floor((distance % day) / hour);
     const minutes = Math.floor((distance % hour) / minute);
     const seconds = Math.floor((distance % minute) / second);
-    console.log(days, hours, minutes, seconds);
+    // console.log(days, hours, minutes, seconds);
 
     //Hide Input
     inputContainer.hidden = true;
@@ -56,8 +56,8 @@ function updateDOM() {
       timeElements[1].textContent = `${hours}`;
       timeElements[2].textContent = `${minutes}`;
       timeElements[3].textContent = `${seconds}`;
+      completeEl.hidden = true;
       countdownEl.hidden = false;
-      countdownEl = false;
     }
   }, second);
 }
@@ -67,15 +67,20 @@ function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
-
-  console.log(countdownTitle, countdownDate);
+  savedCountdown = {
+    title: countdownTitle,
+    date: countdownDate,
+  };
+  console.log(savedCountdown);
+  localStorage.setItem("countdown", JSON.stringify(savedCountdown));
+  // console.log(countdownTitle, countdownDate);
   //Check for valid date
   if (countdownDate === "") {
     alert("Please select a date for the countdown");
   } else {
     //Get number version of current Date, updateDOM
     countdownValue = new Date(countdownDate).getTime();
-    console.log("countdown value:", countdownValue);
+    // console.log("countdown value:", countdownValue);
     updateDOM();
   }
 }
@@ -90,9 +95,25 @@ function reset() {
   //Reset values
   countdownTitle = "";
   countdownDate = "";
+  localStorage.removeItem("countdown");
+}
+
+function restorePreviousCountdown() {
+  //Get countdown from localStorage if available
+  if (localStorage.getItem("countdown")) {
+    inputContainer.hidden = true;
+    savedCountdown = JSON.parse(localStorage.getItem("countdown"));
+    countdownTitle = savedCountdown.title;
+    countdownDate = savedCountdown.date;
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
+  }
 }
 
 //Event Listeners
 countdownForm.addEventListener("submit", updateCountdown);
 countdownBtn.addEventListener("click", reset);
 completeBtn.addEventListener("click", reset);
+
+//On Load, check localStorage
+restorePreviousCountdown();
