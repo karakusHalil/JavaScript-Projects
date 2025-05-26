d0c397a3d03e792ddac2c6a3e686e1b972e74d58
karakusHalil <karakus_halil@outlook.com>
2025-05-27 01:57:33 +0300

    Add dynamic DOM rendering for bookmarks with favicon and delete icon

--- bookmark-app/script.js
+++ bookmark-app/script.js
@@ -46,6 +46,42 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+//Build Bookmarks DOM
+function buildBookmarks() {
+  //Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    console.log(name, url);
+    //Item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    //Close Icon
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fa-solid", "fa-xmark");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark("${url}")`);
+    //Favicon / Link Container
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+    //Favicon
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("alt", "Favicon");
+    //Link
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.append(item);
+  });
+}
+
 //Fetch Bookmarks
 function fetchBookmarks() {
   //Get bookmarks from localStorage if available
@@ -61,7 +97,7 @@ function fetchBookmarks() {
     ];
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
-  console.log(bookmarks);
+  buildBookmarks();
 }
 
 //Handle Data from Form
@@ -82,7 +118,7 @@ function storeBookmark(e) {
     url: urlValue,
   };
   bookmarks.push(bookmark);
-  console.log(bookmarks);
+  //   console.log(bookmarks);
   localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   fetchBookmarks();
   bookmarkForm.reset();
