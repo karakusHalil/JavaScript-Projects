c6291fac6e1fbddf5595358c2f767c14cab3c274
karakusHalil <karakus_halil@outlook.com>
2025-05-26 18:15:19 +0300

    Add modalShow and modalClose function

--- bookmark-app/script.js
+++ bookmark-app/script.js
@@ -5,3 +5,16 @@ const bookmarkForm = document.getElementById("bookmark-form");
 const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
+
+//Show Modal, Focus on Input
+function showModal() {
+  modal.classList.add("show-modal");
+  websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+
+modalShow.addEventListener("click", showModal);
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("show-modal");
+});
