46d4ec6d4f1b04270d5396298cdcb3a6aa3fea9f
karakusHalil <karakus_halil@outlook.com>
2025-05-27 01:29:50 +0300

    Add fetch bookmarks func

--- bookmark-app/script.js
+++ bookmark-app/script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
 
+let bookmarks = [];
+
 //Show Modal, Focus on Input
 function showModal() {
   modal.classList.add("show-modal");
@@ -44,6 +46,24 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+//Fetch Bookmarks
+function fetchBookmarks() {
+  //Get bookmarks from localStorage if available
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    //Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: "karakusHalil",
+        url: "https://github.com/karakusHalil",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  console.log(bookmarks);
+}
+
 //Handle Data from Form
 function storeBookmark(e) {
   e.preventDefault();
@@ -52,11 +72,25 @@ function storeBookmark(e) {
   if (!urlValue.includes("https://") && !urlValue.includes("hhtp://")) {
     urlValue = `https://${urlValue}`;
   }
-  console.log(nameValue, urlValue);
+  //   console.log(nameValue, urlValue);
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  console.log(bookmarks);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 //Event Listener
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+//On Load, Fetch Bookmarks
+fetchBookmarks();
