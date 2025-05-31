9186136b592e2d0ef1c15bd80d79353cfb5aebef
karakusHalil <karakus_halil@outlook.com>
2025-06-01 02:16:41 +0300

    Add player selection and reset previous selection

--- spock-rock-game/script.js
+++ spock-rock-game/script.js
@@ -16,7 +16,7 @@ const computerScissors = document.getElementById("computerScissors");
 const computerLizard = document.getElementById("computerLizard");
 const computerSpock = document.getElementById("computerSpock");
 
-const allGameIcons = document.querySelectorAll("fa-regular");
+const allGameIcons = document.querySelectorAll(".fa-regular");
 
 const choices = {
   rock: { name: "Rock", defeats: ["scissors", "lizard"] },
@@ -25,3 +25,42 @@ const choices = {
   lizard: { name: "Lizard", defeats: ["paper", "spock"] },
   spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
+
+console.log(allGameIcons);
+
+//Reset all "selected" icons
+function resetSelected() {
+  allGameIcons.forEach((icon) => {
+    icon.classList.remove("selected");
+  });
+}
+
+//Passing player selection value and styling icons
+function select(playerChoice) {
+  resetSelected();
+  // Add 'selected' styling & playerChoice
+  switch (playerChoice) {
+    case "rock":
+      playerRock.classList.add("selected");
+      playerChoiceEl.textContent = " --- Rock";
+      break;
+    case "paper":
+      playerPaper.classList.add("selected");
+      playerChoiceEl.textContent = " --- Paper";
+      break;
+    case "scissors":
+      playerScissors.classList.add("selected");
+      playerChoiceEl.textContent = " --- Scissors";
+      break;
+    case "lizard":
+      playerLizard.classList.add("selected");
+      playerChoiceEl.textContent = " --- Lizard";
+      break;
+    case "spock":
+      playerSpock.classList.add("selected");
+      playerChoiceEl.textContent = " --- Spock";
+      break;
+    default:
+      break;
+  }
+}
