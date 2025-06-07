f422a81c3fee67ef363024b60bc8e634e2062612
karakusHalil <karakus_halil@outlook.com>
2025-06-08 00:23:00 +0300

    Add conditional toggle between Results and Favorites views

--- nasa-api-pictures/script.js
+++ nasa-api-pictures/script.js
@@ -13,11 +13,18 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
-function showContent() {
+function showContent(page) {
   window.scrollTo({
     top: 0,
     behavior: "instant",
   });
+  if (page === "results") {
+    resultsNav.classList.remove("hidden");
+    favoritesNav.classList.add("hidden");
+  } else {
+    resultsNav.classList.add("hidden");
+    favoritesNav.classList.remove("hidden");
+  }
   loader.classList.add("hidden");
 }
 
@@ -86,7 +93,7 @@ function updateDOM(page) {
   }
   imagesContainer.textContent = "";
   createDOMNodes(page);
-  showContent();
+  showContent(page);
 }
 
 //Get 10 Images from NASA API
